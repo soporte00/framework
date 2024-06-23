178ece304180b61f0e93749110f54a4a8eb3df08
david <rodavid987@gmail.com>
2024-06-23 13:05:42 -0300

    feat: display error

--- core/kernel_http.php
+++ core/kernel_http.php
@@ -6,6 +6,17 @@
  * 
  */
 
+// hide errors and warnings
+ini_set('display_startup_errors', 0);
+ini_set('display_errors', 0);
+error_reporting(0);
+
+// Show errors if debug
+if (DEBUG) {
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+}
 
 //Global Vars
 define('HTTP', true);
