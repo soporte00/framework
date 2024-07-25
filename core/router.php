c0a76221ff3666850c0b16c15ebaa6a70f70eb3f
david <rodavid987@gmail.com>
2024-07-24 21:09:33 -0300

    feat: spa routes was added

--- core/router.php
+++ core/router.php
@@ -3,21 +3,22 @@
 function route(string $path = '', bool $redirect = false, int $sleepSeconds = 1)
 {
 
-    $protocol = defined('HTTPS') && HTTPS ? 'https://' : 'http://';
-    $url = $protocol . $_SERVER['HTTP_HOST'] . $path;
+	$protocol = defined('HTTPS') && HTTPS ? 'https://' : 'http://';
+	$url = $protocol . $_SERVER['HTTP_HOST'] . $path;
 
-    if ($redirect) {
-        sleep($sleepSeconds);
-        header("Location: " . $url);
-        die();
-    }
+	if ($redirect) {
+		sleep($sleepSeconds);
+		header("Location: " . $url);
+		die();
+	}
 
-    return $url;
+	return $url;
 }
 
 
-function asset($url=null){
-	return route( str_replace('//','/', '/assets/' . $url) );
+function asset($url = null)
+{
+	return route(str_replace('//', '/', '/assets/' . $url));
 }
 
 
@@ -29,12 +30,31 @@ $_controller = isset($_REQUEST[2]) ? $_REQUEST[2] : 'home';
 $_method = isset($_REQUEST[3]) ? $_REQUEST[3] : 'index';
 $_params = isset($_REQUEST[4]) ? $_REQUEST[4] : false;
 
-if($_api){
-    require_once "router-api.php";
-    die();
- }
+if ($_api) {
+	require_once "router-api.php";
+	die();
+}
 
- /**
+
+
+/**
+ * SPA router
+ * 
+ */
+if (SPA) {
+
+	$file = dirname(__FILE__, 2) . "/public/dist/index.html";
+
+	if (!is_file($file)) {
+		core\render::view('default/404.php');
+	}
+
+	echo str_replace('/assets/',  route() . '/assets/',  file_get_contents($file));
+	die();
+}
+
+
+/**
  *
  * controllers instantiation
  *
@@ -42,24 +62,23 @@ if($_api){
 $_file = dirname(__FILE__, 2) . "/src/controllers/" . $_controller . "Controller.php";
 
 
-if (is_readable($_file)){
+if (is_readable($_file)) {
 
 	require $_file;
 
-	$_instance = $_controller.'Controller';
+	$_instance = $_controller . 'Controller';
 	$_instance = new $_instance;
 
-	if(method_exists($_instance,$_method)){
+	if (method_exists($_instance, $_method)) {
 
-		if($_params){
-			call_user_func_array([$_instance, $_method], explode('/',$_params));
+		if ($_params) {
+			call_user_func_array([$_instance, $_method], explode('/', $_params));
 			die();
 		}
 
 		call_user_func([$_instance, $_method]);
 		die();
 	}
-
 }
 
 /**
